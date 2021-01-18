41d323623df556cb83bb26eee8fa5e7e66189686
MauroBraga <mrb0305@gmail.com>
2021-01-18 08:47:04 -0300

    Funcao print e variáveis

---
+++ console.py
@@ -0,0 +1,8 @@
+pais = "Italia";
+
+print(type(pais));
+
+quantidade = 4;
+print(type(quantidade));
+
+print(pais, "ganhou", quantidade, "titulos mundiais");
---
+++ funcPrint_variaveis.py
@@ -0,0 +1,8 @@
+print("Ola mundo");
+
+
+#print("Brasil", "ganhou", 5 ,"titulos", "mundiais", sep="-");
+
+print("Brasil", "ganhou", 5 ,"titulos", "mundiais", sep="\n");
+
+print("Brasil", "ganhou", 5 ,"titulos", "mundiais", end="END");
