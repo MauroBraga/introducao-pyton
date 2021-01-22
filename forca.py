7b47422cbf2ad25ab728dd33c48db6e7a61f4092
MauroBraga <mrb0305@gmail.com>
2021-01-22 16:16:08 -0300

    commit cap 8

---
+++ forca.py
@@ -0,0 +1,7 @@
+
+def jogar():
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
+
+    print("fim do jogo")
