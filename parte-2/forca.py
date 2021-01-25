f88d2724b0138d5352ac16b803638e434755baf7
MauroBraga <mrb0305@gmail.com>
2021-01-25 13:55:30 -0300

    parte 2 início

---
+++ parte-2/forca.py
@@ -0,0 +1,17 @@
+
+def jogar():
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
+    
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
+
+    print("fim do jogo")
+
+if(__name__=="__main__"):
+    jogar()
