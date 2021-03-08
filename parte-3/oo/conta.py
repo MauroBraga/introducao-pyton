cf7ad427e54ea81b6668e3cf5baee4f3296f83bf
Mauro <mrb030584@yahoo.com>
2021-03-08 13:30:28 -0300

    Encapsulamento

--- parte-3/oo/conta.py
+++ parte-3/oo/conta.py
@@ -4,7 +4,20 @@ class Conta:
 
     def __init__(self,numero, titular, saldo, limite):
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de  {} de titular {}".format(self.__saldo,  self.__titular));
+
+    def deposita(self, valor):
+        self.__saldo += valor;
+
+    def sacar(self, valor):
+        self.__saldo -= valor;
+    
+    def transferir(self,valor, destino)
+        self.sacar(valor);
+        destino.deposita(valor);
